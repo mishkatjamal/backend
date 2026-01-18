93e37c07931479751a2c3fd9f80c2a2628ff29a6
mishkatjamal <mishkatjamal13@gmail.com>
2026-01-18 21:24:31 +0530

    create server with express

--- day-1/app.js
+++ day-1/app.js
@@ -1,3 +1,11 @@
-const catMe = require("cat-me");
-catMe();
-console.log(catMe());
+// *cat Me package example
+// const catMe = require("cat-me");
+// catMe();
+// console.log(catMe());
+
+// * express package example
+
+const express = require("express");
+const app = express(); //server ko create kar cuke ho
+
+app.listen(3000);
